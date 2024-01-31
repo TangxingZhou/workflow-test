fe8f975b55ef845de145733385daa31a3c0788dd
txzhou <zhoutangxing@126.com>
2024-01-31 10:32:56 +0800

    test github pages

--- moc-tester/src/tests/test_k8s_api.py
+++ moc-tester/src/tests/test_k8s_api.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.p0
-# @pytest.mark.skip(reason='测试k8s api')
+@pytest.mark.skip(reason='测试k8s api')
 def test_get_all_pods(k8s_api):
     ret = k8s_api.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
@@ -20,7 +20,7 @@ def test_get_all_unit_pods(k8s_unit_client):
         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 @pytest.mark.p0
-# @pytest.mark.skip(reason='测试k8s api')
+@pytest.mark.skip(reason='测试k8s api')
 def test_get_all_controller_pods(k8s_controller_client):
     ret = k8s_controller_client.v1_api.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
