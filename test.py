8d6b28c4170b03c29364ee17a58e2a58fa20cfd7
TangxingZhou <zhoutangxing@126.com>
2024-05-16 16:08:03 +0800

    debug

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import os
 
 sysbench_envs = {
-    'MOC_INSTANCE_HOST': 'restore',
+    'MOC_INSTANCE_HOST': 'restore-freetier-01.cn-hangzhou.cluster.cn-qa.matrixone.tech',
     'MOC_INSTANCE_PORT': 6001,
-    'MOC_INSTANCE_USER': 'root',
-    'MOC_INSTANCE_PASSWORD': '123'
+    'MOC_INSTANCE_USER': '018f7d14_c1fe_76f0_957f_f7c352b4bec6:admin:accountadmin',
+    'MOC_INSTANCE_PASSWORD': 'Admin123'
 }
 for k, v in sysbench_envs.items():
     os.system(f'echo "export {k}={v}" >> .env')
