b3fbe54725b241e1a7c633e1182784da9e0dae3b
TangxingZhou <zhoutangxing@126.com>
2024-08-07 14:07:31 +0800

    test

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ ossFilePath=$5
 parallelism=$6
 baseID=$7
 
-echo "shell args prod freetier-01 registry.cn-hangzhou.aliyuncs.com/mocloud/mo-backup:main-20240731 mo-backup-20240201 backup0717-1520 150"
+# echo "shell args prod freetier-01 registry.cn-hangzhou.aliyuncs.com/mocloud/mo-backup:main-20240731 mo-backup-20240201 backup0717-1520 150"
 echo "new backupID: 019125ba-6147-7d41-a430-84236dadf806"
 echo "backup-id=123456-789-asd" >> $GITHUB_OUTPUT
 echo "backup-id=123456-789-asd" >> $GITHUB_ENV
